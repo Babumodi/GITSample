60d91ae228b353de6d186a0753fc4b48c354c460
szjg35 <szjg35@w2szjg3501.apac.corp.eds.com>
2011-02-16 12:37:55 +0530

    added

---
+++ src/com/sample/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.sample;
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HelloWorld");
+
+	}
+
+}
